fe00822d438029cec74b80cce6651471906acc3a
aiquanpeng <aiquanpeng@163.com>
2022-10-12 22:51:07 +0800

    Dinosaur01

---
+++ src/com/mr/service/FreashThread.java
@@ -0,0 +1,5 @@
+package com.mr.service;
+
+public class FreashThread extends Thread{
+
+}
---
+++ src/com/mr/view/BackgroundImage.java
@@ -0,0 +1,12 @@
+package com.mr.view;
+
+import java.awt.image.BufferedImage;
+
+public class BackgroundImage {
+	public BufferedImage image;
+	
+	static public void roll() {
+		
+	}
+
+}
